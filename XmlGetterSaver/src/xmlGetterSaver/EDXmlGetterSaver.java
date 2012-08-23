d384724687b62ed2cb933f53d21bf21e40cea050
Eyüp <eyupdalan@gmail.com>
2012-08-23 22:45:27 +0300

    properties are created

--- XmlGetterSaver/src/xmlGetterSaver/EDXmlGetterSaver.java
+++ XmlGetterSaver/src/xmlGetterSaver/EDXmlGetterSaver.java
@@ -1,5 +1,34 @@
 package xmlGetterSaver;
 
-public class EDXmlGetterSaver {
+import org.w3c.dom.Document;
 
+public class EDXmlGetterSaver {
+	public Document _xmlDoc;
+	public void setXmlDoc(Document _xmlDoc){
+		this._xmlDoc=_xmlDoc;
+	}
+	public Document setXmlDoc(){
+		return _xmlDoc;
+	}
+	
+	public String _xmlUrl;
+	public void setXmlUrl(String _xmlUrl){
+		this._xmlUrl=_xmlUrl;
+	}
+	public String getXmlUrl(){
+		return _xmlUrl;
+	}
+	
+	public String _xmlLocalPath;
+	public void setXmlLocalPath(String _xmlLocalPath){
+		this._xmlLocalPath=_xmlLocalPath;
+	}
+	
+	public EDXmlGetterSaver(String _xmlUrl){
+		setXmlUrl(_xmlUrl);
+	}
+	
+	public void GetXmlFromUrl(){
+		
+	}
 }
--- XmlGetterSaver/src/xmlGetterSaver/TestClasses.java
+++ XmlGetterSaver/src/xmlGetterSaver/TestClasses.java
@@ -5,3 +5,4 @@ public class TestClasses {
 		System.out.println("Test class!");
 	}
 }
+
