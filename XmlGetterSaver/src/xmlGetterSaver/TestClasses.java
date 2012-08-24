2e61de486942f920b102c5cf55aef8def1e12552
Eyüp <eyupdalan@gmail.com>
2012-08-24 15:15:17 +0300

    work path/not samsung path

--- XmlGetterSaver/src/xmlGetterSaver/TestClasses.java
+++ XmlGetterSaver/src/xmlGetterSaver/TestClasses.java
@@ -7,7 +7,7 @@ public class TestClasses {
 	public static void main(String[] args){
 		System.out.println("Test class!");
 		
-		EDXmlGetterSaver xgs=new EDXmlGetterSaver("http://www.tcmb.gov.tr/kurlar/today.xml","C:\\Users\\edalan.KITAPYURDU\\Desktop\\test.xml");
+		EDXmlGetterSaver xgs=new EDXmlGetterSaver("http://www.tcmb.gov.tr/kurlar/today.xml","C:\\Users\\Eyüp\\Desktop\\test.xml");
 		xgs.GetXmlFromUrl();
 		System.out.println("xml file is taken");
 		xgs.SaveXmlToLocal();
